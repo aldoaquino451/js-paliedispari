a7e3131c668fc4654da0c8966ed7d4b9ad63fba5
aldoaquino451 <aldoaquino2909@gmail.com>
2023-09-15 15:46:25 +0200

    [init] script, style, index file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+
+  1. Salva in una variabile il nome inserito dall'utente
+  2. Ciclo 
+    1. Verifica che il primo e l'ultimo carattere siano ===   
+    2. Verifica che il secondo e il penultimo siano === e via dicendo
+
+*/
+
