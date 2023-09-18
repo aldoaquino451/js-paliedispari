fb65faf6716774b19b15af12c0d2012d35d94bc8
aldoaquino451 <138690750+aldoaquino451@users.noreply.github.com>
2023-09-18 12:59:33 +0200

    [edit] function isEven

--- js/script.js
+++ js/script.js
@@ -1,75 +1,3 @@
-/* 
-1. Chiedi all'utente di scegliere tra pari e dispari 
-2. Chiedi all'utente di scegliere un numero da 1 a 5
-3. Genera un numero per il pc (function)
-4. Somma i numeri
-5. Se la somma è uguale alla scelta dell'utente allora vince lui altrimenti il pc (function)
-*/
-
-// 1. Chiedi all'utente di scegliere tra pari e dispari 
-const choose = prompt('Cosa scegli, "pari" o "dispari"?');
-console.log(choose);
-
-// 2. Chiedi all'utente di scegliere un numero da 1 a 5
-const numUser = parseInt(prompt('Scegli un numero da 1 a 5'));
-console.log(numUser);
-
-// 3. Genera un numero per il pc (function)
-const numComputer = randomize(1, 5);
-
-function randomize (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-console.log(numComputer);
-
-// 4.
-const sum = numUser + numComputer;
-console.log(sum);
-
-// 5. 
-isEven = false;
-
-if ( choose === 'pari' ) {
-
-  if ( sum % 2 === 0) {
-    console.log('user win');
-  }
-  else {
-    console.log('pc win');
-  }
-
-}
-else if ( choose === 'dispari' ) {
-
-  if ( sum % 2 === 0) {
-    console.log('pc win');
-  }
-  else {
-    console.log('user win');
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 // 1. Salva in una variabile il nome inserito dall'utente
 const paliStr = (prompt('Inserisci una parola'));
@@ -101,3 +29,60 @@ else {
   paliHTML.innerHTML = paliStr + ' NON è una parola  palindroma';
 }
 */
+
+/* 
+4. Somma i numeri
+5. Se la somma è uguale alla scelta dell'utente allora vince lui altrimenti il pc (function)
+*/
+
+
+// 1. Chiedi all'utente di scegliere tra pari e dispari  un numero da 1 a 5
+const choose = prompt('Cosa scegli, "pari" o "dispari"?');
+const numUser = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+// 2. Genera un numero per il pc (tramite funzione)
+const numComputer = randomize(1, 5);
+
+function randomize (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// 3. Somma i numeri
+const sum = numUser + numComputer;
+console.log(choose, numUser, numComputer, sum);
+
+// 4. 
+message = (isEven(sum)) ? 
+
+
+// 5. 
+function isEven(num) {
+  if ( num % 2 ) return false;
+  else return true;
+};
+
+
+// 6.
+let message;
+
+const evenOdd = document.querySelector('#even-odd')
+
+evenOdd.innerHTML = message;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
